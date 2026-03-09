1db18957d48c9c71100a92e2d2e776304996d538
songWooin <sowintildeblue@gmail.com>
2026-03-09 16:27:48 +0900

    test:엔드포인트

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Flask 서버가 실행 중입니다!"
+
+# 새로 추가하는 API 엔드포인트
+@app.route('/api/hello')
+def api_hello():
+    return jsonify({"message": "Success!"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
