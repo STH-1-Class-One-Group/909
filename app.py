d4850a1dc3d942665267c1621c7d7a0d934bf4c4
김수빈 <bz.subin@gmail.com>
2026-03-10 13:04:18 +0900

    feat: debug 추가

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ def POST_choose():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
+#debug=True 서버 새로고침 안 해도 자동 반영
