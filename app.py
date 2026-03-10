3045cab88c89f8af8bbc818e5cdc2d38c36869da
김수빈 <bz.subin@gmail.com>
2026-03-10 14:10:39 +0900

    fix: route 수정

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def index():
 
 @app.route('/go_get', methods=["GET"]) #GET 요청용 , /go_get 저요!!!
 def GET_choose():
-    return jsonify({"m" : "get 받음!!"})
+    return jsonify({"message" : "get 받음!!"})
 
 
 @app.route('/go_post', methods=["POST"]) #POST 요청용
 def POST_choose():
-    return jsonify({"m" : "post 받음!!"})
+    return jsonify({"message":"post 받음!!"})
 
 
 if __name__ == "__main__":
