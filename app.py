ae8541160fc32de8536bbad463fa7db6247dfdbf
1968-501 <samesamechan0412@gmail.com>
2026-03-10 12:29:41 +0900

    add: POST 완료, 미완성

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import os
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS  
 
 app = Flask(__name__)
@@ -20,7 +20,12 @@ def get_test():
 
 @app.route("/post_test", methods=["POST"])
 def post_test():
-    return "post_test 연습"    
+    request_data = request.get_json()
+    real_msg = request_data.get("msg")
 
+    return jsonify({
+        "message": f"'{real_msg}"
+    })
+    
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
