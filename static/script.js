496a3babcbefa37772fd73ff8bed9cd37a869582
김수빈 <bz.subin@gmail.com>
2026-03-17 18:05:33 +0900

    docs: 주석 삭제

--- static/script.js
+++ static/script.js
@@ -3,19 +3,20 @@ const user_text = document.querySelector("#user-text")     // content 받는 inp
 const save_db_button = document.querySelector("#save-db-button")   // 저장 버튼
 const get_db_button = document.querySelector("#get-db-button")     // DB 내용 가져오는 버튼
 const DB_plus_div = document.querySelector(".DB-plus-div")         // 받은 DB 데이터를 출력하기 위한 영역(위치) 
-//// 불러오면서 .value 쓰면 새로고침 할 때 마다 들어온 값을 변수에 저장함
 const img_user_input = document.querySelector("#img-user-input")   // 저장 버튼
+
+
 
 
 //* 이벤트 리스너(save_db_button) - input 값 가져옴
 save_db_button.addEventListener("click", async(event) =>{
     event.preventDefault()
     const live_user_text = user_text.value 
-    console.log("버튼 클릭", live_user_text)
-//// live_user_text : user_text의 값(버튼 클릭 시 입력된 값)을 저장
+    console.log("저장 버튼 클릭", live_user_text)
+// 저장 버튼을 누르면 입력한 내용의 값을(input) 변수에 저장
 
     //* fetch.  db_create를 연결점으로 정보를 주고(input 값) 받음(return 값)
-    const response = await fetch("/db_create", {  // db_create 어딨니! 
+    const response = await fetch("/feeds_input_save", {  // db_create 어딨니! 
         method : "POST",
         headers : { "Content-Type": "application/json" }, 
         body: JSON.stringify({ "message": live_user_text }) //* 보낼거
@@ -24,9 +25,8 @@ save_db_button.addEventListener("click", async(event) =>{
     const json_response = await response.json() //* 받은거
     console.log(json_response)
 })
-//// event.preventDefault() // 이 친구는 form 방해꾼임. 일 못 하게 괴롭힘
-//// response : input 보내고 받은 답 (db_create 함수 return 값)
-//// json_response :  response를 json으로 바꿈(DB 저장 확인)
+//// 버튼 이벤트 감지 -> 사용자가 input에 입력한걸(content) 가져와서 
+//// 그걸 python에 보내고, 잘 받았다는 값을 받는 것
 
 
 
@@ -53,8 +53,7 @@ get_db_button.addEventListener("click", async (event) => {
         console.error("데이터 가져오기 실패!", error);
     }
 });
-//// receive : 받은 정보(DB)
-//// json_receive : receive를 json으로 바꿈(db 정보)
+//// 데이터 베이스에 있는 내용을 읽어서 출력(반복문 사용하여 줄줄히 출력)
 
 
 
