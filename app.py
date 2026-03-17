b05550ac0f25468db085fddb67a5e0d585d59974
김수빈 <bz.subin@gmail.com>
2026-03-17 14:50:27 +0900

    fix: 포트 번호 수정

--- app.py
+++ app.py
@@ -92,6 +92,6 @@ def db_read():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5909))
     app.run(host="0.0.0.0", port=port, debug=True)
 #debug=True 서버 새로고침 안 해도 자동 반영
