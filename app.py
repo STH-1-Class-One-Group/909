8f28a519c8fa9d4420d6063fe803a02311195d0c
김수빈 <bz.subin@gmail.com>
2026-03-18 17:34:49 +0900

    feat: 엔드 포인트 부분 삭제

--- app.py
+++ app.py
@@ -134,16 +134,6 @@ def map_page():
 def community_page(place_name):
     return render_template('community.html', place_name=place_name) 
 
-# 새로 추가하는 API 엔드포인트
-@app.route('/api/hello')
-def api_hello():
-    return jsonify({"message": "Success!"})
-
-@app.route('/api/fail')
-def api_fail():
-    # 404 에러와 함께 메시지 전송
-    return jsonify({"message": "요청하신 페이지를 찾을 수 없습니다!"}), 404
-
 
 
 if __name__ == "__main__":
